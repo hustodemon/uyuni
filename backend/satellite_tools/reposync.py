7c22f1d58909f0052b7e4a2b825d18d4fade95f3
Frantisek Kobzik <fkobzik@suse.de>
2021-01-25 10:03:21 +0100

    Revert "Reposync: treat resources using 'with'"
    This reverts commit b7af15e8fe4ee58e16fefae92d8263fa47f110b9.

--- backend/satellite_tools/reposync.py
+++ backend/satellite_tools/reposync.py
@@ -743,10 +743,14 @@ class RepoSync(object):
 
     @staticmethod
     def _get_decompressed_file_checksum(abspath, hashtype):
-        with fileutils.decompress_open(abspath) as src, tempfile.TemporaryFile('w') as tmp:
-            shutil.copyfileobj(src, tmp)
-            tmp.flush()
-            return getFileChecksum(hashtype, fd=tmp.fileno())
+        src = fileutils.decompress_open(abspath)
+        tmp = tempfile.TemporaryFile('w')
+        shutil.copyfileobj(src, tmp)
+        tmp.flush()
+        result = getFileChecksum(hashtype, fd=tmp.fileno())
+        tmp.close()
+        src.close()
+        return result
 
     def copy_metadata_file(self, plug, filename, comps_type, relative_dir):
         old_checksum = None
