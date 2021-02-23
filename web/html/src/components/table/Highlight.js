a4a6eed155808737672ec1a8c2b5368f2dfc5a1b
Etheryte <karl.tarvas@suse.com>
2021-02-23 18:46:44 +0100

    Migrate cveaudit, highlight and related fixes

--- web/html/src/components/table/Highlight.js
+++ web/html/src/components/table/Highlight.js
@@ -1,13 +1,14 @@
-// @flow
-import * as React from 'react';
+import * as React from "react";
 
 type HighlightProps = {
   /** text to display */
-  text: string,
+  text: string;
+
   /** substring to search in the text and highlight */
-  highlight: string,
+  highlight?: string;
+
   /** flag enabling highlighting */
-  enabled?: boolean,
+  enabled?: boolean;
 };
 
 /** Search and highlight part of a text */
@@ -15,25 +16,35 @@ export function Highlight(props: HighlightProps) {
   let text = props.text;
   let high = props.highlight;
 
-  if (!props.enabled) {
-    return <span key="hl">{text}</span>
+  if (!props.enabled || !high) {
+    return <span key="hl">{text}</span>;
   }
 
   let pos = text.toLocaleLowerCase().indexOf(high.toLocaleLowerCase());
   if (pos < 0) {
-    return <span key="hl">{text}</span>
+    return <span key="hl">{text}</span>;
   }
 
-  let chunk1 = text.substring(0, pos);
-  let chunk2 = text.substring(pos, pos + high.length);
-  let chunk3 = text.substring(pos + high.length, text.length);
+  let chunk1: React.ReactNode = text.substring(0, pos);
+  let chunk2: React.ReactNode = text.substring(pos, pos + high.length);
+  let chunk3: React.ReactNode = text.substring(pos + high.length, text.length);
 
   chunk1 = chunk1 ? <span key="m1">{chunk1}</span> : null;
-  chunk2 = chunk2 ? <span key="m2" style={{borderRadius: "2px"}}><mark>{ chunk2 }</mark></span> : null;
+  chunk2 = chunk2 ? (
+    <span key="m2" style={{ borderRadius: "2px" }}>
+      <mark>{chunk2}</mark>
+    </span>
+  ) : null;
   chunk3 = chunk3 ? <span key="m3">{chunk3}</span> : null;
 
-  return <span key="hl">{chunk1}{chunk2}{chunk3}</span>;
+  return (
+    <span key="hl">
+      {chunk1}
+      {chunk2}
+      {chunk3}
+    </span>
+  );
 }
 Highlight.defaultProps = {
   enabled: false,
-}
+};
